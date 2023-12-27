1b9003554c8f97509738a4fd82f3495ac34bfb0a
Gwinyai <gtchax@users.noreply.github.com>
2023-12-27 21:54:25 +0200

    Added mongo dn url and e2e script

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-const DEV_URI = process.env.DB 
 
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(DEV_URI as string);
+    const connectionInstance = await mongoose.connect(process.env.DB_URL as string);
     console.log(
       `\n MongoDB connected | DB HOST: ${connectionInstance.connection.host}`
     );
--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,25 @@ import "dotenv/config";
 import "express-async-errors";
 import { userRouter } from "./routes/users";
 import { errorHandler, notFound } from "./middleware/error";
+import connectDB from "./config/db";
 
+connectDB();
 const app = express();
 const PORT = process.env.PORT || 8090;
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/users", userRouter);
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log("Server running on port ");
+  console.log(`Server running on port ${PORT}`);
 });
