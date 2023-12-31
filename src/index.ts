15af79faacc642a5318db6b0f69cbf63b2d4f2ad
Gwinyai <gtchax@users.noreply.github.com>
2023-12-31 23:19:52 +0200

    Added cloudinary

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
-import path from 'path'
+import path from "path";
 import cors from "cors";
 import "dotenv/config";
 import "express-async-errors";
 import { userRouter } from "./routes/users";
 import { errorHandler, notFound } from "./middleware/error";
 import connectDB from "./config/db";
+import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 connectDB();
 const app = express();
 const PORT = process.env.PORT || 8090;
@@ -19,7 +25,7 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../../client/dist')))
+app.use(express.static(path.join(__dirname, "../../client/dist")));
 app.use("/api/v1/users", userRouter);
 app.use(notFound);
 app.use(errorHandler);
